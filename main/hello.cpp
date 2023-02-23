1fcd8d80b41f3001adedfa36c33a7224d2ed405a
Greyhair360 <115022689+Greyhair360@users.noreply.github.com>
2023-02-24 00:27:04 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Neeraj-PES2UG20CS527_Jenkins\n";
+  cout << "Hello, EDITED Neeraj-PES2UG20CS527_Jenkins\n";
   cout << "I have been successfully built and run\n";
   cout << "Ikkuzo";
   return 0;
